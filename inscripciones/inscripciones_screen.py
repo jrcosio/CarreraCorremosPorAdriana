ceef3e0b32dc9272273db8e630bb3005a2077a84
Pablo Blanco <pablo.blancoc1@gmail.com>
2025-05-19 20:38:23 +0200

    actualizado_inscripciones/errores_corregidos

--- inscripciones/inscripciones_screen.py
+++ inscripciones/inscripciones_screen.py
@@ -1,11 +1,21 @@
 import flet as ft
 from .cont_campos import Campos
+from barra_navegacion import NavBar
 
 class InscripcionScreen(ft.Container):
     def __init__(self):
         super().__init__(
-            content=Campos()
+            alignment=ft.alignment.center,
+            expand=True,
+            content=ft.Column(
+                controls=[
+                    NavBar(),
+                    Campos()
+                ],
+                scroll=ft.ScrollMode.AUTO,
+                expand=True,
+            ),
         )
-
+            
 if __name__ == "__main__":
     print("Esta clase no se puede ejecutar de forma independiente.")
