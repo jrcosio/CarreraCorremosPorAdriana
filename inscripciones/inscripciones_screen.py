e82693eccc5b43e5d5f05d6780c568aff723da5b
Pablo Blanco <pablo.blancoc1@gmail.com>
2025-05-24 20:34:56 +0200

    centrando...

--- inscripciones/inscripciones_screen.py
+++ inscripciones/inscripciones_screen.py
@@ -5,17 +5,8 @@ from barra_navegacion import NavBar
 class InscripcionScreen(ft.Container):
     def __init__(self):
         super().__init__(
-            alignment=ft.alignment.center,
-            expand=True,
-            content=ft.Column(
-                controls=[
-                    NavBar(),
-                    Campos()
-                ],
-                scroll=ft.ScrollMode.AUTO,
-                expand=True,
-            ),
+            content=Campos()
         )
-            
+
 if __name__ == "__main__":
     print("Esta clase no se puede ejecutar de forma independiente.")
