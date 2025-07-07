079b597e771542bb10ef8e84475533f31bf4a5b5
Jose Gutierrez Cossio <jolugucos@gmail.com>
2025-07-07 09:45:41 +0200

    medidascamposclasificacion

--- clasificacion/clasificacion_screen.py
+++ clasificacion/clasificacion_screen.py
@@ -36,14 +36,14 @@ COLORES = {
 }
 
 MEDIDAS = {
-    "puesto":60,
-    "dorsal": 60,
+    "p.": 40,
+    "d.": 40,
     "nombre": 100,
     "apellidos": 200,
     "sexo": 45,
-    "ccaa": 60,
-    "categoria": 80,
-    "tiempo": 100,
+    "ca": 45,
+    "cat.": 80,
+    "tiempo_final": 100,
     "gap": 80,
     "ritmo": 80,
 }
@@ -62,11 +62,11 @@ class ClasificacionScreen(ft.Container):
 
     def _cargar_datos_iniciales(self):
         try:
-            self.clasificacion = self.bd.obtener_clasificacion_por_edicion(self.edicion)
-            if self.clasificacion:
-                # Obtener el tiempo del primer clasificado para calcular el gap
-                self.tiempo_ganador = self.clasificacion[0].tiempo
-            log.info(f"Cargados {len(self.clasificacion)} clasificados")
+            self.clasificacion = self.bd.obtener_clasificaciones_por_edicion(self.edicion)
+            # if self.clasificacion:
+            #     # Obtener el tiempo del primer clasificado para calcular el gap
+            #     self.tiempo_ganador = self.clasificacion[0].tiempo
+            # log.info(f"Cargados {len(self.clasificacion)} clasificados")
         except Exception as e:
             log.error(f"Error cargando clasificación: {e}")
             self.clasificacion = []
@@ -136,7 +136,7 @@ class ClasificacionScreen(ft.Container):
             controls=[
                 ft.Container(height=10),
                 ft.Text(
-                    f"Clasificación Trail Peñasagra",
+                    f"Clasificación Trail Sierra de Peñasagra",
                     size=40,
                     color=COLORES["titulo"],
                     font_family="Britanic Bold",
@@ -156,14 +156,14 @@ class ClasificacionScreen(ft.Container):
 
     def _crear_encabezados_tabla(self):
         encabezados = [
-            ("Puesto", MEDIDAS["puesto"], ft.alignment.center),
-            ("Dorsal", MEDIDAS["dorsal"], ft.alignment.center),
+            ("P.", MEDIDAS["p."], ft.alignment.center),
+            ("D.", MEDIDAS["d."], ft.alignment.center),
             ("Nombre", MEDIDAS["nombre"], ft.alignment.center_left),
             ("Apellidos", MEDIDAS["apellidos"], ft.alignment.center_left),
             ("Sexo", MEDIDAS["sexo"], ft.alignment.center),
-            ("CCAA", MEDIDAS["ccaa"], ft.alignment.center_left),
-            ("Categoria", MEDIDAS["categoria"], ft.alignment.center),
-            ("Tiempo", MEDIDAS["tiempo"], ft.alignment.center),
+            ("CA", MEDIDAS["ca"], ft.alignment.center),
+            ("Cat.", MEDIDAS["cat."], ft.alignment.center),
+            ("Tiempo", MEDIDAS["tiempo_final"], ft.alignment.center),
             ("Gap", MEDIDAS["gap"], ft.alignment.center),
             ("Ritmo", MEDIDAS["ritmo"], ft.alignment.center),
         ]
@@ -187,27 +187,32 @@ class ClasificacionScreen(ft.Container):
 
     def _crear_filas_datos(self):
         return [
-            self._crear_fila_clasificado(clasificado)
-            for clasificado in self.clasificacion
+            self._crear_fila_clasificado(clasificado, index)
+            for  index, clasificado in enumerate(self.clasificacion)
         ]
+    
+           
 
-    def _crear_fila_clasificado(self, clasificado):
+    def _crear_fila_clasificado(self, clasificado, index):
         # Calcular el gap dinámicamente
-        gap_calculado = self._calcular_gap(clasificado.tiempo)
+       # gap_calculado = self._calcular_gap(clasificado.tiempo)
         
         return ft.Container(
             content=ft.Row(
                 controls=[
-                    self._crear_celda_datos(str(clasificado.puesto), MEDIDAS["puesto"], ft.alignment.center),
-                    self._crear_celda_datos(str(clasificado.inscrito.dorsal), MEDIDAS["dorsal"], ft.alignment.center),
+                    self._crear_celda_datos(str(index + 1), MEDIDAS["p."], ft.alignment.center),
+                    self._crear_celda_datos(str(clasificado.inscrito.dorsal), MEDIDAS["d."], ft.alignment.center),
                     self._crear_celda_datos(clasificado.inscrito.nombre, MEDIDAS["nombre"], ft.alignment.center_left),
                     self._crear_celda_datos(clasificado.inscrito.apellidos, MEDIDAS["apellidos"], ft.alignment.center_left),
                     self._crear_celda_datos(clasificado.inscrito.sexo, MEDIDAS["sexo"], ft.alignment.center),
                     self._crear_celda_ccaa(clasificado.inscrito.ccaa),
-                    self._crear_celda_datos(clasificado.inscrito.categoria, MEDIDAS["categoria"], ft.alignment.center),
-                    self._crear_celda_datos(clasificado.tiempo, MEDIDAS["tiempo"], ft.alignment.center),
-                    self._crear_celda_datos(gap_calculado, MEDIDAS["gap"], ft.alignment.center),
-                    self._crear_celda_datos(clasificado.ritmo, MEDIDAS["ritmo"], ft.alignment.center),
+                    self._crear_celda_datos(
+                        self._calcular_categoria(clasificado.inscrito.fecha_nacimiento), 
+                                 MEDIDAS["cat."], 
+                        ft.alignment.center),
+                    self._crear_celda_datos(clasificado.tiempo_final, MEDIDAS["tiempo_final"], ft.alignment.center),
+                    self._crear_celda_datos("gap", MEDIDAS["gap"], ft.alignment.center),
+                    self._crear_celda_datos("rit", MEDIDAS["ritmo"], ft.alignment.center),
                 ],
                 alignment=ft.MainAxisAlignment.CENTER,
                 spacing=10,
@@ -219,26 +224,49 @@ class ClasificacionScreen(ft.Container):
 
     def _crear_celda_datos(self, texto, ancho, alineacion):
         return ft.Container(
-            content=ft.Text(str(texto), color="#ffcb2e", size=16),
+            content=ft.Text(str(texto), color="#ffcb2e", size=20),
             padding=2,
             width=ancho,
             alignment=alineacion,
         )
 
-    def _crear_celda_ccaa(self, ccaa):
+    def _crear_celda_ccaa(self, ca):
         return ft.Container(
             content=ft.Row(
                 controls=[
                     ft.Image(
-                        src=BANDERAS_CCAA.get(ccaa),
+                        src=BANDERAS_CCAA.get(ca),
                         fit=ft.ImageFit.CONTAIN,
                         width=30,
                         height=20,
                     ),
                     # ft.Text(ccaa, size=16, color="#ffcb2e"),
                 ],
-                alignment=ft.MainAxisAlignment.START,
+                alignment=ft.MainAxisAlignment.CENTER,
             ),
-            width=MEDIDAS["ccaa"],
-            alignment=ft.alignment.center_left,
+            width=MEDIDAS["ca"],
+            alignment=ft.alignment.center,
         )
+        
+    def _calcular_categoria(self, fecha_nacimiento):
+        """Calcula la categoría basada en la fecha de nacimiento."""
+        try:
+            year = fecha_nacimiento.year
+            
+            categorias = [
+                (1975, "VET C"),
+                (1979, "VET B"),
+                (1985, "VET A"),
+                (2007, "SENIOR"),
+                (float('inf'), "JUNIOR"),
+            ]
+            
+            for limite_año, categoria in categorias:
+                if year <= limite_año:
+                    return categoria
+                    
+            return "N/A"
+            
+        except Exception as e:
+            log.error(f"Error calculando categoría para fecha {fecha_nacimiento}: {e}")
+            return "N/A"
